b8a62920a11569d9c70a2efc42e24243b6f25c5f
thaolt <thaolt@songphi.com>
2020-08-06 20:51:30 +0700

    update docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,19 @@
 
 php artisan migrate
 
+ln -s /srv/www/storage/app/public /srv/www/public/storage
+
+chown nginx:nginx /srv/www/storage
+chown nginx:nginx /srv/www/storage/app
+chown nginx:nginx /srv/www/storage/app/public
+
+
+chown nginx:nginx -R /srv/www/storage/app/db-blade-compiler
+chown nginx:nginx -R /srv/www/storage/debugbar
+chown nginx:nginx -R /srv/www/storage/framework
+chown nginx:nginx -R /srv/www/storage/logs
+
+
 echo "Starting up.."
 
 supervisord -c /etc/supervisord.conf
